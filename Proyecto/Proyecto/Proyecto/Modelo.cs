b4a537b440c342d53578d5d6f87e4e440169eb71
Román <rpc.2024@gmail.com>
2020-12-02 17:36:43 +0100

    si o que

---
+++ Proyecto/Proyecto/Proyecto/Modelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto
+{
+    class Modelo
+    {
+        public static int PotenciaBase2(int n)
+        {
+            int i = 2;
+
+            i = i * 2;
+            return 2 * i;
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Proyecto/Proyecto/Proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
